f653fd92c05b291e29ed7a89b4fb3df644436c7e
AndrielyBona <106356904+AndrielyBona@users.noreply.github.com>
2022-10-07 17:09:52 -0300

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,6 +12,11 @@ import { FooterComponent } from './components/footer/footer.component';
 import { FormsModule } from '@angular/forms';
 import { ListFurnitureClientComponent } from './components/list-furniture-client/list-furniture-client.component';
 import { ClientFurnitureComponent } from './components/client-furniture/client-furniture.component';
+import { AddFurnitureComponent } from './components/add-furniture/add-furniture.component';
+import { AreaLivingComponent } from './components/area-living/area-living.component';
+import { ClientProfileComponent } from './components/client-profile/client-profile.component';
+import { FilterWoodworksComponent } from './components/filter-woodworks/filter-woodworks.component';
+import { FoundWoodWorkComponent } from './components/found-wood-work/found-wood-work.component';
 
 
 
@@ -28,7 +33,6 @@ import { ClientFurnitureComponent } from './components/client-furniture/client-f
     HeaderLoginComponent,
     HeaderWoodworkComponent,
     HomePageComponent,
-
     FooterComponent,
     ListFurnitureClientComponent,
     ClientFurnitureComponent
