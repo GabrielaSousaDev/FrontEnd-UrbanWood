19517aaebeb9d97fa7e3c9fa64a92663fd0e7431
AndrielyBona <106356904+AndrielyBona@users.noreply.github.com>
2022-10-14 13:58:03 -0300

    Update

---
+++ src/app/models/allClient.ts
@@ -0,0 +1,16 @@
+export interface allClient{
+    idClient: number,
+    nameClient: String,
+    lastName: String,
+    cpfClient: String,
+    nameStreet: String,
+    number: number,
+    complement: String,
+    neighborhood: String,
+    city: String,
+    cep: String,
+    socialNetwork: String,
+    email: String,
+    phone: String;
+
+}
