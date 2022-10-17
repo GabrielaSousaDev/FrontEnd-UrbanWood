fa355363e5b5b7c265c621bb42b7846b9f626324
AndrielyBona <106356904+AndrielyBona@users.noreply.github.com>
2022-10-17 17:08:09 -0300

    Update

--- src/app/models/furniture.ts
+++ src/app/models/furniture.ts
@@ -1,11 +1,11 @@
 export interface Furniture{
     idFurniture: number,
     name: string,
-    price: number, //
+    price: number, 
     description: string,
-    SizeFurniture: string, //
-    environment: string //
-    color: string //
-    image: string //
+    SizeFurniture: string, 
+    environment: string 
+    color: string 
+    image: string 
     id_woodwork: number
 }
