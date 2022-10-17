0a8d2d066625452c7776c8b716ad6210adfc3c43
Eduardo Pereira <eduardopereirafausto@gmail.com>
2022-10-17 16:11:41 -0300

    Update

--- src/app/service/woodwork.service.ts
+++ src/app/service/woodwork.service.ts
@@ -10,32 +10,31 @@ export class WoodworkService {
 
   woodworkRefresh = new EventEmitter<string>()
 
-  endPointClient: string = 'http://localhost:8080/woodwork/'
+  endPointWoodwork: string = 'http://localhost:8080/woodwork/'
 
   constructor(private http: HttpClient) {}
 
   //  POST  //
 
   addWoodwork(postData: allWoodwork): Observable<allWoodwork> {
-    return this.http.post<allWoodwork>(this.endPointClient, postData);
+    return this.http.post<allWoodwork>(this.endPointWoodwork, postData);
   }
 
   //  GET  //
 
   getWoodwork(id: number): Observable<allWoodwork> {
-    return this.http.get<allWoodwork>(this.endPointClient + id);
+    return this.http.get<allWoodwork>(this.endPointWoodwork + id);
   }
 
   //  PUT  //
 
   updateWoodwork(postData: number): Observable<allWoodwork> {
-    return this.http.put<allWoodwork>(this.endPointClient, postData);
+    return this.http.put<allWoodwork>(this.endPointWoodwork, postData);
   }
 
   // DELETE //
   
   deleteWoodowork(id: number): Observable<allWoodwork> {
-    return this.http.delete<allWoodwork>(this.endPointClient + id);
+    return this.http.delete<allWoodwork>(this.endPointWoodwork + id);
   }
-
 }
